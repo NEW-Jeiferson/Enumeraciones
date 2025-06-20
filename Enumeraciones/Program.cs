ca2b4925758ddc4b534db7a09f25e90831d85693
David Paez <jeffersonpaez5@gmail.com>
2025-06-20 18:28:31 -0400

    Add project files.

---
+++ Enumeraciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+enum DiasSemana
+{
+    Lunes,
+    Martes,
+    Miercoles,
+    Jueves,
+    Viernes,
+    Sabado,
+    Domingo
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DiasSemana hoy = DiasSemana.Miercoles;
+       Console.WriteLine("Hoy Es: " + hoy);
+
+        int ValorNumerico = (int)hoy;
+        Console.WriteLine("Hoy Es: " + hoy + "Es: " + ValorNumerico);
+
+        DiasSemana otroDia = (DiasSemana)4;
+        Console.WriteLine("El Dia 4 Es: " + otroDia);
+
+        Console.ReadKey();
+    }
+}
